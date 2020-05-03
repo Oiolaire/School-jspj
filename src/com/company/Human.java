75ef950c322d3bae7143a7d509d96a0269b828b7
Oiolaire <49201065+Oiolaire@users.noreply.github.com>
2020-05-03 12:33:32 +0200

    Zadanie 3 i 4: get your salary & give yourself a rise

--- src/com/company/Human.java
+++ src/com/company/Human.java
@@ -6,6 +6,42 @@ public class Human {
     public String lastName;
     public Animal pet;
 
+    private Car car;
+
+    private Double salary;
+
+    public void setCar(Car car) {
+        if(salary*5>car.cena) {
+            System.out.println("Samochód gupiony za gotówke");
+            this.car = car;
+        }
+        else if(salary*10>car.cena){
+            System.out.println("Samochód kupiony na kredyt");
+            this.car = car;
+        }
+        else{
+            System.out.println("Na samochód cię nie stać cię biedaku");
+        }
+    }
+
+    public void getCar() {
+        System.out.println("I'm driving : " + car);
+    }
+
+    public void setSalary(Double salary) {
+        if (salary <= 1000) {
+            System.out.println("Come on have more faith in yourself (new salary is below legal limit)");
+        } else {
+            System.out.println("Sending new data to the accountants");
+            System.out.println("Make sure to send documentation to Ms. Hania");
+            System.out.println("ZUS and US already know");
+            System.out.println("Your new salary: " + salary);
+            this.salary = salary;
+        }
+    }
 
 
+    public Double getSalary() {
+        return salary;
+    }
 }
